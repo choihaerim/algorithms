787749290fb7eb903261e29374db6105028c3b63
choihaerim <87001457+choihaerim@users.noreply.github.com>
2022-01-11 12:35:39 +0900

    문제 추가

--- 코딩테스트/1-1.py
+++ 코딩테스트/1-1.py
@@ -1,5 +1,21 @@
-#행운의 번호는 2*n 자릿수를 가진 숫자인데, 왼쪽에서부터 n 개의 숫자의 합이 오른쪽에서부터 n 개의 숫자의 합과 같다.
-#당신에게 0을 제외한 숫자로만 이루어진 문자열 **s**가 주어진다. 이 문자열의 부분문자열 중 가장 긴 행운의 번호의 길이를 구하시오. 만약 행운의 번호가 없다면 0을 반환하시오.
+"""
+재미있는 게임을 해보자.
+"is there anybody out there"이라는 문자열을 빈칸으로 구분한 각 단어들의 길이를 써보면
+2, 5, 7, 3, 5이 된다.
+'1'부터 '9'로만 이루어진 문자열
+**string number**
+와 문자열 배열
+**vector<string> dictionary**
+이 주어진다.
+우리는 이 두개의 데이터를 가지고 문장을 만들어 내야 한다.
+number의 각 요소 number[i]는 i번째로 올 단어의 길이를 나타낸다.
+dictionary의 각 요소는 단어를 나타낸다.
+예를들어 입력이 아래와 같다고 가정해보면
+num
+dictionary = ["is","there","anybody","out","there"]
+"is there anybody out there"이 리턴값이 된다. 만약 같은 길이의 단어를 골라야 된다면, 알파벳 순서로 먼저 있는것을 우선하여 고른다.
+이때, 주어지는 number와 dictionary에 따라 올바른 답을 반환하시오.
+"""
 
 class Solution:
     def solution(self, number, dictionary):
