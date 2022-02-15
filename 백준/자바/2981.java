128e6c3f27ec18d14305cf3b036938e7b6f63637
choihaerim <87001457+choihaerim@users.noreply.github.com>
2022-02-15 15:25:40 +0900

    2981

---
+++ 백준/자바/2981.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+ 
+public class Main {	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+		
+		int[] arr = new int[N];	
+        
+		for(int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+        
+		Arrays.sort(arr);	
+ 
+		int gcdVal = arr[1] - arr[0];
+ 
+		for(int i = 2; i < N; i++) {
+			gcdVal = gcd(gcdVal, arr[i] - arr[i - 1]);
+		}
+ 
+		for(int i = 2; i <= gcdVal; i++) {
+	    
+
+			if(gcdVal % i == 0) {
+				System.out.print(i + " ");
+			}
+		}
+		
+	}
+	
+	static int gcd(int a, int b) {
+		while(b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+ 
+}
