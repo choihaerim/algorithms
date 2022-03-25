428d35abe5dcfb7d795d6769d8922e9def437e3a
seaweeed <haerimchoi19@gmail.com>
2022-03-25 09:47:14 +0900

    2805

--- 백준/자바/Main.java
+++
@@ -1,4 +0,0 @@
-public class Main {
-    public static void main(String[] args) { int i = 1; System.out.println(++i); // 먼저 + System.out.println(i++); // 나중 + System.out.println(i--); // 나중 - System.out.println(--i); // 먼저 - 
-}
-
