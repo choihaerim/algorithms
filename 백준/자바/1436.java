122aa77821e019ea16608867524a638924d5c7ab
seaweeed <haerimchoi19@gmail.com>
2022-04-06 10:40:05 +0900

    2231

---
+++ 백준/자바/1436.java
@@ -0,0 +1,14 @@
+int N = input(); // 입력
+ 
+int num = 666;
+int count = 1;
+ 
+while(count != N) {
+	num++;
+    
+	// int형인 num을 String으로 변환한 뒤, "666"이란 문자열이 있는지 검사
+	if(String.valueOf(num).contains("666")) { 
+		count++;
+	}
+}
+print(num);
