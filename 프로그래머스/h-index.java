47b5b43b22e7703a24009a20f680c74511cd83f5
seaweeed <haerimchoi19@gmail.com>
2022-05-08 09:23:46 +0900

    h-index

---
+++ 프로그래머스/h-index.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class HIndex {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int length = citations.length;
+        int h = 0;
+        int k = 0;
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+            h = citations[i];
+            k =  citations.length - i;
+            if (k <= h) {
+                answer = k;
+                break;
+            }
+        }
+        return answer;
+    }
+}
